f6f664546627f45c013d081991fc8344870ff5f1
ahmet <codingwithmish@gmail.com>
2024-02-24 18:43:38 +0300

    added link href in navbar logo and github icon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,9 +10,9 @@ export default function Navbar() {
   return (
     <div className="pt-4">
       <nav className="flex items-center justify-between">
-        <h2 className="text-3xl font-bold">Blog<span className="text-primary">mish</span></h2>
+        <Link href='/'><h2 className="text-3xl font-bold">Blog<span className="text-primary">mish</span></h2></Link>
         <div className="gap-3 flex items-center">
-          <Link href='/' className="flex items-center justify-center w-10 h-10 border-[1px] rounded-md hover:bg-gray-100 transition-all"><Github size={18} /></Link>
+          <Link href='https://github.com/codingwithmish/blogmish' className="flex items-center justify-center w-10 h-10 border-[1px] rounded-md hover:bg-gray-100 dark:hover:bg-slate-800 transition-all"><Github size={18} /></Link>
           <ThemeToggle />
         </div>
       </nav>
